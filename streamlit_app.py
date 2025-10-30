42e6ed68c06e557dd67df66948ae1520fcfd0bd1
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-10-30 12:08:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def load_data():
     
     # Create a new column with the full, absolute image URL
     # The original filename is in the 'Image' column
-    df['images'] = IMAGE_BASE_URL + df['id']
+    df['images'] = IMAGE_BASE_URL + df['id'] + 'png'
     return df
 
 data = load_data()
